9ff016c8f52953b581ee5b79389134096f6758e2
Sam Thorold <sam.thorold@gmail.com>
2022-03-09 02:33:15 +0000

    Refactor

---
+++ tests/booker/test_core.py
@@ -0,0 +1,44 @@
+from datetime import date
+
+import pytest
+
+from booker.core import Account, Entry, Sign
+
+
+def test_entry_is_immutable():
+    e = Entry(
+        ref="001",
+        date=date(2022, 1, 1),
+        sign=Sign.DEBIT,
+        amount=100,
+    )
+
+    # dataclasses.FrozonInstanceError is subclass of AttributeError
+    with pytest.raises(AttributeError):
+        e.amount = 200
+
+
+def test_entry_hashable():
+    e = Entry(
+        ref="001",
+        date=date(2022, 1, 1),
+        sign=Sign.DEBIT,
+        amount=100,
+    )
+    hash(e) 
+
+
+def test_no_entry_type_conversion():
+    e = Entry(
+        ref="001",
+        date=date(2022, 1, 1),
+        sign=1,
+        amount=100,
+    )
+
+    assert isinstance(e.sign, int), type(e.sign)
+
+
+def test_init_account_with_no_entries():
+    _ = Account(code="1", name="a")
+
