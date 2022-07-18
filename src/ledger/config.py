0c594b1da2fe7ca5aa2c61ceb8c279aeb93bdc80
Sam Thorold <sam.thorold@gmail.com>
2022-07-18 23:21:13 +0100

    Tox not running

--- src/ledger/config.py
+++ src/ledger/config.py
@@ -5,7 +5,7 @@ def get_postgres_uri():
     host = os.environ.get("DB_HOST", "localhost")
     port = 54321 if host == "localhost" else 5432
     password = os.environ.get("DB_PASSWORD", "abc123")
-    user, db_name = "allocation", "allocation"
+    user, db_name = "ledger", "ledger"
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
